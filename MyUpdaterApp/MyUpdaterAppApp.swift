df7a76af20762faa4d71fcf670ef5cb5f11afc40
SevenAndrew <82178604+SevenAndrew@users.noreply.github.com>
2024-12-21 19:46:34 +0100

    Initial Commit

---
+++ MyUpdaterApp/MyUpdaterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyUpdaterAppApp.swift
+//  MyUpdaterApp
+//
+//  Created by Andreas Sauerwein on 21.12.24.
+//
+
+import SwiftUI
+
+@main
+struct MyUpdaterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
